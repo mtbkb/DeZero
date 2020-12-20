75c0cd60ca36cdf7b530faf1ac95e1a6f9fa04fe
mtbkb <bk.mountain.dvlp11@gmail.com>
2020-12-20 19:44:35 +0900

    add  step02

--- steps/step02.py
+++ steps/step02.py
@@ -9,9 +9,12 @@ class Variable:
 class Function:
     def __call__(self, input):
         x = input.data
-        y = x ** 2
+        y = self.forward(x)
         output = Variable(y)
         return output
+
+    def forward(self, x):
+        return x ** 2
 
 
 x = Variable(np.array(10))
