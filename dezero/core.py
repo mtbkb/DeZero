486469aee565537901199b0a691bd5305f3a9a4c
mtbkb <bk.mountain.dvlp11@gmail.com>
2021-01-05 23:50:25 +0900

    add some funct

--- dezero/core.py
+++ dezero/core.py
@@ -1,6 +1,7 @@
 import numpy as np
 import weakref
 import contextlib
+import dezero
 
 
 class Variable:
@@ -58,6 +59,9 @@ class Variable:
                     for y in f.outputs:
                         y().grad = None
     
+    def transpose(self):
+        return dezero.functions.transpose(self)
+
     @property
     def shape(self):
         return self.data.shape
@@ -73,6 +77,10 @@ class Variable:
     @property
     def dtype(self):
         return self.data.dtype
+    
+    @property
+    def T(self):
+        return dezero.functions.transpose(self)
     
     def cleargrad(self):
         self.grad = None
@@ -115,6 +123,14 @@ class Variable:
     
     def __pow__(self, other):
         return pow(self, other)
+    
+    def reshape(self, *shape):
+        if len(shape) == 1 and isinstance(shape[0], (tuple, list)):
+            shape = shape[0]
+        return dezero.functions.reshape(self, shape)
+    
+    def sum(self, axis=None, keepdims=False):
+        return dezero.functions.sum(self, axis, keepdims)
 
 
 class Function:
--- dezero/functions.py
+++ dezero/functions.py
@@ -1,5 +1,34 @@
 import numpy as np
 from dezero.core import Function
+from dezero.core import as_variable
+from dezero import utils
+
+
+class Reshape(Function):
+    def __init__(self, shape):
+        self.shape = shape
+    
+    def forward(self, x):
+        self.x_shape = x.shape
+        y = x.reshape(self.shape)
+        return y
+    
+    def backward(self, gy):
+        return reshape(gy, self.x_shape)
+
+
+class Transpose(Function):
+    def forward(self, x):
+        y = np.transpose(x)
+        return y
+    
+    def backward(self, gy):
+        gx = transpose(gy)
+        return gx
+
+
+def transpose(x):
+    return Transpose()(x)
 
 
 class Sin(Function):
@@ -35,6 +64,66 @@ class Tanh(Function):
         return gx
 
 
+class Sum(FUnction):
+    def __init__(self, axis, keepdims):
+        self.axis = axis
+        self.keepdims = keepdims
+    
+    def forward(self, x):
+        self.x_shape = x.shape
+        y = x.sum(axis=self.axis, keepdims=self.keepdims)
+        return y
+    
+    def backward(self, gy):
+        gy = utils.reshape_sum_backward(gy, self.x_shape, self.axis, self.keepdims)
+        gx = broadcast_to(gy, self.x_shape)
+        return gx
+
+
+def sum(x, axis=None, keepdims=False):
+    return Sum(axis, keepdims)(x)
+
+class SumTo(Function):
+    def __init__(self, shape):
+        self.shape = shape
+    
+    def forward(self, x):
+        self.x_shape = x.shape
+        y = utils.sum_to(x, self.shape)
+        return y
+    
+    def backward(self, gy):
+        gx = broadcast_to(gy, self.x_shape)
+        return gx
+
+
+def sum_to(x, shape):
+    if x.shape == shape:
+        return as_variable(x)
+    return SumTo(shape)(x)
+
+
+class BroadcastTo(Function):
+    def __init__(self, shape):
+        self.shape = shape
+    
+    def forward(self, x):
+        self.x_shape = x.shape
+        y = np.broadcast_to(x, self.shape)
+        return y
+    
+    def backward(self, gy):
+        gx = sum_to(gy, self.x_shape)
+        return gx
+
+
+def broadcast_to(x, shape):
+    if x.shape == shape:
+        return as_variable(x)
+    
+    return BroadcastTo(shape)(x)
+
+
 def sin(x):
     return Sin()(x)
 
@@ -45,4 +134,10 @@ def cos(x):
 
 def tanh(x):
     return Tanh()(x)
+
+
+def reshape(x, shape):
+    if x.shape == shape:
+        return as_variable(x)
+    return Reshape(shape)(x)
 
