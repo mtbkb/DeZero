6693f101cd2f1af388f4fca1b8e0633a615e941a
mtbkb <62200944+mtbkb@users.noreply.github.com>
2021-02-18 16:59:55 +0900

    Revert "Feature/test/2"

--- steps/step34.py
+++ steps/step34.py
@@ -31,8 +31,3 @@ plt.show()
 
 
 print("test commit")
-print("follow the reaper")
-print("commmit on feature branch")
-print("second commit on feature branch")
-print("third commit on feature branch")
-print("fourth commit ")
